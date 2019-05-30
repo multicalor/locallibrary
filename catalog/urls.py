12a83ad4d525929e4d64627f856dc735b7ac21ad
Tretiuk Dmitriy <d.tretiuk@gmail.com>
2019-05-31 00:49:24 +0300

    FirstModels

---
+++ catalog/urls.py
@@ -0,0 +1,13 @@
+#from django.urls import path
+#from django.urls import include
+
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('catalog/', include('catalog.urls')),
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+]
