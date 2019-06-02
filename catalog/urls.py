be326498209735ae534f6eef55ec65fa7cc33be6
Tretiuk Dmitriy <d.tretiuk@gmail.com>
2019-06-02 14:46:00 +0300

    Manual Part 6: Displaying Lists and Detailed Information

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,8 +6,12 @@
 #    path('catalog/', include('catalog.urls')),
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    #url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
